34bfe91e495feabf36d463a689d25e268d818c42
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-06-07 23:40:14 +0500

    socket apply

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken';
 import http from 'http';
 import app from './app.js';
 import {Server} from 'socket.io';
+import mongoose from "mongoose";
+import projectModel from "./models/project.model.js";
 
 const server=http.createServer(app);
 
@@ -16,9 +18,16 @@ const io =new Server(server,{
     }
 });
 
-io.use((socket,next)=>{
+io.use(async(socket,next)=>{
     try {
         const token=socket.handshake.auth.token || socket.handshake.headers.authorization?.split(' ')[1];
+        const projectId=socket.handshake.query.projectId;
+        if(!mongoose.Types.ObjectId.isValid(projectId)){
+           return next(new Error('Invalid project id')); 
+        }
+        socket.project=await projectModel.findById(projectId)
+
+
         if(!token){
             return next(new Error('Authentication error'));
         }
@@ -36,7 +45,15 @@ io.use((socket,next)=>{
 
 
 io.on('connection', socket => {
+    socket.roomId=socket.project._id.toString()
     console.log('A user connected');
+    console.log(socket.project._id.toString());
+    socket.join(socket.roomId)
+    socket.on('project-message',data=>{
+        console.log(data);
+        socket.to(socket.roomId).emit('project-message',data);
+    })
+
     socket.on('event', data => { /* … */ });
     socket.on('disconnect', () => { /* … */ });
 });
