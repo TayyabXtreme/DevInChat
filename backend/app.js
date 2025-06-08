5a106650927596d80fa0627a14f42248c99685c1
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-06-08 23:20:53 +0500

    app

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import connectDB from "./db/db.js";
 import UserRoutes from "./routes/user.routes.js";
 import ProjectRoutes from "./routes/project.routes.js";
+import AiRoutes from "./routes/ai.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/users",UserRoutes);
 app.use('/projects',ProjectRoutes);
+app.use('/ai',AiRoutes);
 
 
 app.get("/",(req,res)=>{
